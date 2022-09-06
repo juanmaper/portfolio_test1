64030f9c3b8a90248b053b79224abe2d718f1c2b
Juanma <juanmaper30@gmail.com>
2022-09-06 17:42:34 +0200

    Base for dialog messages

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,14 +10,18 @@
     
     <q-header class="header">     
       <div class="row justify-left"> 
-        <q-tabs align="right" no-caps class="col-10" indicator-color="transparent">
+        <q-tabs align="right" no-caps class="col-11" indicator-color="transparent">
+
+          <q-tab active-class="active-route" class="circularTabs q-mt-xl q-mr-md" 
+            name="aboutMe" label="Sobre mí" @click="alert = true"
+          />
 
           <q-route-tab active-class="active-route" class="circularTabs q-mt-xl q-mr-md" 
-            name="aboutMe" label="Sobre mí" to="/" exact
+            name="projects" label="Proyectos" to="projects" exact
           />
 
           <q-route-tab active-class="active-route" class="circularTabs q-mt-xl q-mr-xl" 
-            name="projects" label="Proyectos" to="projects" exact
+            name="contactme" label="Contáctame" 
           />
 
         </q-tabs> 
@@ -26,6 +30,21 @@
     </q-header>
     
     <q-page-container>
+      <q-dialog v-model="alert">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
       <!-- <router-view /> -->
     </q-page-container>
   </q-layout>
@@ -44,7 +63,7 @@ export default defineComponent({
 
 
     return {
-      
+      alert: ref(false),
     }
 
   }
