8eb1b9cd928bfe1b52d738648a8b446d03b23b5e
Juanma <juanmaper30@gmail.com>
2022-09-07 18:22:04 +0200

    Test q img to fit container

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,11 @@
 <template>
-  <video
-        src="backgroundVideo.mp4" muted
-        autoplay
-        loop />
+  <div class="container absolute-center">
+    <q-img src="~assets/personalImage.jpg" height="100%" width="100%" />
+  </div>
 
-  <h1 class="text-h3 centerText absolute-center matriverRegularFont">I am Juanma</h1>
+  <!-- <h1 class="text-h3 centerText absolute-center matriverRegularFont">I am Juanma</h1> -->
 
-  <q-layout view="lHh Lpr lFf">
+  <!-- <q-layout view="lHh Lpr lFf">
     
     <q-header class="header">     
       <div class="row justify-left"> 
@@ -17,7 +16,7 @@
           />
 
           <q-route-tab active-class="active-route" class="circularTabs q-mt-xl q-mr-md" 
-            name="projects" label="Proyectos" to="projects" exact
+            name="projects" label="Proyectos"
           />
 
           <q-route-tab active-class="active-route" class="circularTabs q-mt-xl q-mr-xl" 
@@ -30,24 +29,9 @@
     </q-header>
     
     <q-page-container>
-      <q-dialog v-model="alert">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Alert</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn flat label="OK" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-      <!-- <router-view /> -->
+       <router-view /> 
     </q-page-container>
-  </q-layout>
+  </q-layout> -->
   
 </template>
 
@@ -72,20 +56,6 @@ export default defineComponent({
 
 <style scoped lang="scss">
 
-.header {
-  background-color: rgba(0, 0, 0, 0);
-}
-
-.circularTabs {
-  border-radius: 50px;
-  border: 1px solid #fff;
-  background-color: $vue-primary;
-}
-
-.active-route {
-  background-color: $vue-primary;
-  border: 1px solid #fff;
-}
 
 .centerText {
   // background-color: red;
@@ -94,11 +64,17 @@ export default defineComponent({
   text-align: center;
 }
 
-video {
-
-  min-width: 100%;
-  min-height: 100%;
+.container {
+  height: 500px;
+  width: 500px;
+  background-color: red;
 }
+
+video {
+  height: 700px;
+  width: 100%;
+}
+
 
 
 
